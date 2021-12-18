183351a78df02f7b0163e1bfa78ca09a6becdc58
manab oz <staff@oz-edu.com>
2021-12-18 12:40:57 +0900

    First commit

---
+++ RealmSwift/RealmSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmSwiftApp.swift
+//  RealmSwift
+//
+//  Created by 辻大地 on 2021/12/18.
+//
+
+import SwiftUI
+
+@main
+struct RealmSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
